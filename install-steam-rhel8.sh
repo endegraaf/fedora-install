3d409cbd75abed2ce100959d26974e0fb36e0d5c
Eric de Graaf <endegraaf@gmail.com>
2021-03-23 19:57:53 +0100

    Add files via upload

---
+++ install-steam-rhel8.sh
@@ -0,0 +1,10 @@
+sudo dnf install  json-glib.i686 libdbusmenu.i686 fontconfig.i686 freetype.i686 glib2.i686 gtk2.i686 pango.i686 glibc.i686
+wget https://negativo17.org/repos/steam/epel-7/x86_64/libdbusmenu-gtk2-16.04.0-4.el7.i686.rpm
+rpm -U libdbusmenu-gtk2-16.04.0-4.el7.i686.rpm --nodeps
+sudo dnf install  libdrm.i686
+sudo dnf install  https://download1.rpmfusion.org/free/el/updates/8/x86_64/l/libva-intel-driver-2.1.0-4.el8.x86_64.rpm
+sudo dnf install  alsa-lib.i686 alsa-plugins-pulseaudio.i686 libXScrnSaver.i686 libXtst.i686 libatomic.i686 libcurl.i686 libdbusmenu-gtk3.i686 libpng12.i686 libvdpau.i686
+sudo dnf install  mesa-dri-drivers.i686 mesa-libGL.i686 nss.i686
+sudo dnf install  https://download1.rpmfusion.org/free/el/rpmfusion-free-release-8.noarch.rpm
+sudo dnf install  https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-8.noarch.rpm
+sudo dnf install  steam

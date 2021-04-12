6231dd3d198a60a437f67250306ffcdbf4f19fd6
Eric de Graaf <endegraaf@gmail.com>
2021-04-12 23:38:58 +0200

    Update install-dnf.sh

--- install-dnf.sh
+++ install-dnf.sh
@@ -2,7 +2,7 @@ sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-r
 
 sudo dnf -y groupinstall "Development libraries" "Java Development"
 #sudo dnf -y groupinstall "KDE Plasma Workspaces" libtool
-sudo dnf install -y sdl2-devel ffms2-devel meson gcc make java-devel ark gradle  gradle gnome-builder vim-enhanced remmina inkscape kompare vlc podman android-tools obs-studio kdenlive audacity krita inkscape chromium gstreamer{1,}-{ffmpeg,libav,plugins-{good,ugly,bad{,-free,-nonfree}}} --setopt=strict=0 steam spectacle syncthing
+sudo dnf install -y sdl2-devel ffms2-devel meson gcc make java-devel ark gradle  gradle gnome-builder vim-enhanced remmina inkscape kompare vlc podman android-tools obs-studio kdenlive audacity krita inkscape chromium gstreamer{1,}-{ffmpeg,libav,plugins-{good,ugly,bad{,-free,-nonfree}}} --setopt=strict=0 steam spectacle syncthing emacs
 
 # Fonts
 sudo dnf install -y ibm-plex-fonts-all

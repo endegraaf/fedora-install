a0327769e86399181d9c0fc619f8e24393876865
Eric de Graaf <endegraaf@gmail.com>
2021-04-12 22:56:20 +0200

    Update remove the KDE desktop stuff.

--- install-dnf.sh
+++ install-dnf.sh
@@ -1,11 +1,5 @@
 sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-sudo dnf -y groupinstall "Development libraries"
-sudo dnf -y groupinstall "Java Development"
-sudo dnf -y groupinstall "KDE Plasma Workspaces" libtool
-sudo dnf install -y sdl2-devel ffms2-devel meson gcc make java-devel ark gradle ksshaskpass gradle gnome-builder photoflare plasma-workspace-wayland vim-enhanced
-sudo dnf install -y remmina inkscape kompare vlc podman android-tools kde-connect livecd-tools 
-sudo dnf install -y obs-studio kdenlive audacity synfig krita inkscape latte-dock chromium
-sudo dnf install -y gstreamer{1,}-{ffmpeg,libav,plugins-{good,ugly,bad{,-free,-nonfree}}} --setopt=strict=0
-sudo dnf install -y steam copyq ksnip syncthing syncthing-cli syncthing-tools
-
+sudo dnf -y groupinstall "Development libraries" "Java Development"
+#sudo dnf -y groupinstall "KDE Plasma Workspaces" libtool
+sudo dnf install -y sdl2-devel ffms2-devel meson gcc make java-devel ark gradle  gradle gnome-builder vim-enhanced remmina inkscape kompare vlc podman android-tools obs-studio kdenlive audacity krita inkscape chromium gstreamer{1,}-{ffmpeg,libav,plugins-{good,ugly,bad{,-free,-nonfree}}} --setopt=strict=0 steam spectacle syncthing

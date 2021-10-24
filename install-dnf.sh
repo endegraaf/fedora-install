0360b6b52f6ce3ff999a686570f12779a13c6420
Eric de Graaf <egraaf@thinkpad.local>
2021-10-24 21:15:29 +0200

    update dnf install

--- install-dnf.sh
+++ install-dnf.sh
@@ -1,11 +1,7 @@
 sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
+sudo dnf update 
 sudo dnf -y groupinstall "Development libraries" "Java Development"
-#sudo dnf -y groupinstall "KDE Plasma Workspaces" libtool
-sudo dnf install -y sdl2-devel ffms2-devel meson gcc make java-devel ark gradle  gradle gnome-builder vim-enhanced remmina inkscape kompare vlc podman android-tools obs-studio kdenlive audacity krita inkscape chromium gstreamer{1,}-{ffmpeg,libav,plugins-{good,ugly,bad{,-free,-nonfree}}} --setopt=strict=0 steam spectacle syncthing emacs
-
-# Fonts
+sudo dnf install -y sdl2-devel ffms2-devel meson gcc make java-devel ark gradle gnome-builder vim-enhanced remmina inkscape kompare vlc podman android-tools obs-studio kdenlive audacity krita inkscape chromium gstreamer{1,}-{ffmpeg,libav,plugins-{good,ugly,bad{,-free,-nonfree}}} --setopt=strict=0 steam spectacle syncthing emacs
+sudo dnf remove -y totem
 sudo dnf install -y ibm-plex-fonts-all
-
-#reload font cache
 sudo fc-cache -f -v
